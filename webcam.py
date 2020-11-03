878e9fef26f6f1af7edfb8e266dfa0a1b3244c3c
RickRobey <73867946+RickRobey@users.noreply.github.com>
2020-11-02 22:17:21 -0600

    Add files via upload

---
+++ webcam.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import cv2
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    cv2.imshow('Hello Rick! Press "q" to quit', frame)
+    if cv2.waitKey(10) == ord('q'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
+
+
